2b6adc839c1e748c616f3060baef258f18969f92
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2025-03-20 21:45:25 -0300

    centralized more functions to the utils file

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -1,10 +1,11 @@
 const md5 = require('md5');
 
-
-
 import { hashPassword } from '@/app/utils/database';
 
 const apiKey = process.env.NEXT_PUBLIC_DB_API;
+const token = "Bearer "+process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
+// ============ GET DATA ============ //
 
 async function checkClientCPF(cpf){
   var url = process.env.NEXT_PUBLIC_DB_URL+`getClienteCPF.php?api_key=${apiKey}&cpf=${cpf}`;
@@ -40,162 +41,155 @@ async function getClientCPF(cpf){
   })
 }
 
-
-
-
-
-async function registerClient(nome, email, cpf, cep, telefone, salt, hash){
-  // !$nome || !$email || !$cpf || !$cep || !$telefone || !$salt || !$hash
-
-
+async function getAllClient() {
+  const url = process.env.NEXT_PUBLIC_DB_URL+`listAll.php?api_key=${apiKey}`;
     
-    const urlCheck = process.env.NEXT_PUBLIC_DB_URL+`getClienteCPF.php?api_key=${apiKey}&cpf=${cpf}`;
-    
-    await fetch(urlCheck)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error(`Erro: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then((data) => {
-      //setData(data)
-      //console.log(data)
-      var count = Object.keys(data).length / 4;
-      //console.log(count)
-      //console.log(cpf)
-      //console.log(email)
-      
-      if(count >= 1){
-        setCpfError('Já existe outro cadastro com este cpf')
-        return
+  await fetch(url)
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error(`Erro: ${response.status}`);
       }
-    })
-    .catch((error) => {
-        // CREATE CLIENTE
+      return response.json();
+  })
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => console.log(error.message));
+}
 
-        var senha = hashPassword(password); // [salt,hash]
-        var salt = senha[0]
-        var hash = senha[1]
+async function getComprasClient(cpf) {
+    
+  const url = process.env.NEXT_PUBLIC_DB_URL+`getComprasCliente.php?api_key=${apiKey}&cpf=${cpf}`;
+  
+  await fetch(url)
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error(`Erro: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => console.log(error.message));
+}
 
+async function getAdmin(login) {
+  const url = process.env.NEXT_PUBLIC_DB_URL+`getAdmin.php?api_key=${apiKey}&login=${login}`;
+    
+  await fetch(url)
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error(`Erro: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => console.log(error));
+}
 
-        const clienteData = {apiKey, nome, email, cpf, cep, telefone, salt, hash};
-        const token = process.env.NEXT_PUBLIC_DB_URL;
-        const url = token+`createCliente.php`;
+// ============ CREATE DATA ============ //
 
-        //console.log(url)
-        //console.log(clienteData)
+async function createCliente(nome, email, cpf, cep, telefone, password) {
+  
+  var senha = hashPassword(password); // [salt,hash]
+  var salt = senha[0]
+  var hash = senha[1]
 
-        fetch(url, {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(clienteData),
-        })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Erro: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-          //console.log(data)
+  var url = process.env.NEXT_PUBLIC_DB_URL+`createCliente.php`;
 
-          const token = "Bearer "+process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-          //create preference
-          const myHeaders = new Headers();
-          myHeaders.append("Content-Type", "application/json");
-          myHeaders.append("Authorization", token);
-          const raw = JSON.stringify({
-            "auto_return": "approved",
-            "back_urls": {
-              "success": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Sucesso",
-              "failure": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Erro",
-              "pending": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Erro"
-            },
-            "statement_descriptor": "Camarote - Se Voce Nao For Eu Vou",
-            "items": [
-              {
-                "id": "010983098",
-                "title": "Ingresso - Se você não for eu vou",
-                "quantity": quant,
-                "unit_price": 360,
-                "description": "Ingresso + kit(Camisa, Caneca, Pulseira)"
-              }
-            ],
-            "payer": {
-              "email": email,
-              "name": nome
-            },
-            "payment_methods": {
-              "excluded_payment_types": [{ id: "ticket" },],
-              "excluded_payment_methods": [{ id: "bolbradesco" },],
-              "installments": 1
-            },
-            "notification_url": process.env.NEXT_PUBLIC_DB_URL+"notification.php",
-            "external_reference": cpf,
-            "expires": false
-          });
-          const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-          };
+  var clienteData = {apiKey, nome, email, cpf, cep, telefone, salt, hash};
 
-          //console.log(raw)
+  fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(clienteData),
+  })
+  .then((response) => {
+      if (!response.ok) {
+          throw new Error(`Erro: ${response.status}`);
+      }
+      return response.json();
+  })
+  .then((data) => {
+    return data;
+  })
+  .catch((error) => console.log('erro cadastro cliente'));
 
-          fetch("https://api.mercadopago.com/checkout/preferences", requestOptions)
-            .then((response) => response.json())
-            .then((result) => {
+}
 
-              setInitPoint(result.init_point)
+async function createPreference(nome, email, cpf, quant){
+  
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", token);
+  const raw = JSON.stringify({
+    "auto_return": "approved",
+    "back_urls": {
+      "success": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Sucesso",
+      "failure": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Erro",
+      "pending": process.env.NEXT_PUBLIC_DB_URL_RETURN+"Erro"
+    },
+    "statement_descriptor": "Camarote - Se Voce Nao For Eu Vou",
+    "items": [
+      {
+        "id": "010983098",
+        "title": "Ingresso - Se você não for eu vou",
+        "quantity": quant,
+        "unit_price": 360,
+        "description": "Ingresso + kit(Camisa, Caneca, Pulseira)"
+      }
+    ],
+    "payer": {
+      "email": email,
+      "name": nome
+    },
+    "payment_methods": {
+      "excluded_payment_types": [{ id: "ticket" },],
+      "excluded_payment_methods": [{ id: "bolbradesco" },],
+      "installments": 1
+    },
+    "notification_url": process.env.NEXT_PUBLIC_DB_URL+"notification.php",
+    "external_reference": cpf,
+    "expires": false
+  });
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
 
-              // console.log(cpf)
-              // console.log(result)
-              // console.log(result.id)
-              // console.log(result.init_point)
+  fetch("https://api.mercadopago.com/checkout/preferences", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
 
-              var idPreference = result.id;
-              //const link = result.init_point;
-              const link = result.init_point;
+    setInitPoint(result.init_point)
 
-              //console.log(idPreference)
-              const apiKey = process.env.NEXT_PUBLIC_DB_API;
-              const preferenceData = {apiKey, idPreference, cpf, quant};
+    const link = result.init_point;
 
-              //console.log(preferenceData)
-              //console.log(clienteData)
-              const url2 = process.env.NEXT_PUBLIC_DB_URL+`createPreference.php`;
+    window.location.href = link;
+  })
+  .catch((error) => console.error('erro api mercado pago'));
+}
 
-              fetch(url2, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(preferenceData),
-              })
-              .then((response) => {
-                  if (!response.ok) {
-                      throw new Error(`Erro: ${response.status}`);
-                  }
-                  return response.json();
-              })
-              .then((data) => {
-                // console.log(data)
-                // console.log('consegui cadastrar cliente e preference')
-                // console.log(link)
-                window.location.href = link;
-              })
-              .catch((error) => console.error('erro cadastro preferencia'));
-            })
-            .catch((error) => console.error('erro api mercado pago'));
-        })
-        .catch((error) => console.log('erro cadastro cliente'));
+// ============ COMPLEX FLOW ============ //
 
-        //console.log(error.message)
-    });
+async function registerClient(nome, email, cpf, cep, telefone, password){
+
+  var check = checkClientCPF(cpf);
+
+  if(check){
+    try {
+      createCliente(nome, email, cpf, cep, telefone, password);
+    } catch (error) {
+      
+    }
+  }
 
 }
 
